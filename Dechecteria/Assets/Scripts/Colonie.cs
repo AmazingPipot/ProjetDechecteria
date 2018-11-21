35bf186562f5eba9f6718846abb2d02e359e7df3
Jessica <delanux.jessica@gmail.com>
2018-11-21 16:56:22 +0100

    add rooms +  background in scene colonie

--- Dechecteria/Assets/Scripts/Colonie.cs
+++ Dechecteria/Assets/Scripts/Colonie.cs
@@ -1,25 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class Colonie : MonoBehaviour {
 
     float[,] dataColonie = new float[40, 10];
+    public float time = 0.0f;
 
     /* 
      * Propriétés des colonies
     */
-    public int energieMax; // = energie max de la colonie
-    public int energie;// = energie de la colonie
+    public static int energieMax = 1000; // = energie max de la colonie
+    public int energie = energieMax / 2;// = energie de la colonie
 
-    public float vitesse;// = Vitesse colonie
-    public int attaque;// = Attaque colonie
-    public int defense;// = Defense colonie
+    public float vitesse = 0.3f;// = Vitesse colonie
+    public int attaque = 0;// = Attaque colonie
+    public int defense = 0;// = Defense colonie
 
     /*
      * Quantité de déchets stockés en fonction des types
     */
-    public int Rorganique;// = Stocke matiere organique
+    //public int Rorganique;// = Stocke matiere organique
     public int Rmineral;// = Stocke verre
     public int Rmetal;// = Stocke métaux
     public int Rchimique;// = Stocke produit chimique
@@ -33,18 +34,18 @@ public class Colonie : MonoBehaviour {
     /*
      * Pièce de stockage des déchets élémentaires
     */
-    public int Porganique;// = piece stockage matiere organique
-    public int Pmineral;// = piece stockage verre
-    public int Pmetal;// = piece stockage métal
-    public int Pchimique;// = piece stockage produit chimique
-    public int Ppetrole;// = piece stockage petrole
-    public int Pnucleaire;// = piece stockage nucleaire
+    public OrganiqueRoom Porganique;// = piece stockage matiere organique
+    public MineralRoom Pmineral;// = piece stockage verre
+    public MetalRoom Pmetal;// = piece stockage métal
+    public ChimiqueRoom Pchimique;// = piece stockage produit chimique
+    public PetrolRoom Ppetrole;// = piece stockage petrole
+    public NuclearRoom Pnucleaire;// = piece stockage nucleaire
 
     /*
      * Les déchets composés sont tous stockés dans une unique pièce avant leur décomposition
      * en déchets élémentaires dans les pièces dédiées
     */
-    public int Pcomplexe;// = piece de stockage dechet complexe, papier, plastique, ...
+    public ComplexRoom Pcomplexe;// = piece de stockage dechet complexe, papier, plastique, ...
 
     public int PseparationComplexe;//piece de transformation en déchets complexes (voiture --> metal+plastique+petrole+matiere organique)
     public int PseparationPapier;//Pièce transformation papier --> produit chimique, matière organique
@@ -74,15 +75,32 @@ public class Colonie : MonoBehaviour {
     public int Vplastique;// = vitesse absorption plastique
     public int Vcomplexe;// = vitesse absorption complexe
 
- 
 
     // Use this for initialization
     void Start () {
-		
-	}
+        StartCoroutine(TimerTick());
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+    }
+
+    // Par défaut, à toutes les secondes, energie decremente de 1
+    IEnumerator TimerTick()
+    {
+        while (time >= 0)
+        {
+            //attendre 1 seconde
+            yield return new WaitForSeconds(1.0f);
+            time++;
+            energie--;
+            Debug.Log("Time " + time.ToString() + " energie " + energie); ;
+        }
+    }
+
+    void RecupereDechets()
+    {
+        // Si on est sur une Tile contenant dechets organiques
+
+    }
 }
