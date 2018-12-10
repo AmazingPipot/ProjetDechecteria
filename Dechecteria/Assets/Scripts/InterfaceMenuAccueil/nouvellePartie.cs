f1f40f0f6b32c84964790ab234725640491cab58
AmazingPipot <quentin.leroit@gmail.com>
2018-12-10 12:16:19 +0100

    debut menu accueil, intro au jeu

---
+++ Dechecteria/Assets/Scripts/InterfaceMenuAccueil/nouvellePartie.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nouvellePartie : MonoBehaviour {
+    public GameObject NewPartie;
+    public GameObject ControllerMenu;
+
+    public void LancementPartie()
+    {
+        NewPartie.SetActive(true);
+        ControllerMenu.SetActive(false);
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
